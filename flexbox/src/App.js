9410539094c60ec3b411a15fcc206d5e6baec751
Akanksha Patel <a.patel@easternenterprise.com>
2021-09-26 21:14:35 +0530

    basic flexbox properties

---
+++ flexbox/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+    <div className="box box1">1</div>
+    <div className="box box2">2</div>
+    <div className="box box3">3</div>
+    <div className="box box4">4</div>
+    <div className="box box5">5</div>
+    <div className="box box6">6</div>
+    <div className="box box7">7</div>
+    <div className="box box8">8</div>
+    <div className="box box9">9</div>
+    <div className="box box10">10</div>
+  </div>
+  );
+}
+
+export default App;
